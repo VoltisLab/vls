8b1ea1ebaa455ff2caa2d01520e7325577d6d27c
Stanley Samuel <157316198+ArikpoStanley@users.noreply.github.com>
2025-08-23 06:45:51 -0400

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='max-w-[75vw] mx-auto'>
+    <div className='max-w-[75vw] mx-auto w-full'>
         <AboutBody2
       leftTitle="About Voltis Labs Games"
       paragraphs={[
